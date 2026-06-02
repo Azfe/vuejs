a6cd3e454cb981fc3871f3c33fe310b25bb9bdc9
Alex Zapata <alexzapata1984@gmail.com>
2026-06-02 14:46:13 +0200

    Add component GameCounter

---
+++ src/components/GameCounter.vue
@@ -0,0 +1,23 @@
+<script setup>
+
+console.log('componente de juego')
+
+
+</script>
+
+<template>
+  <button @click="counter++">+</button>
+
+  <span>contador {{ counter }}</span>
+
+  <button @click="counter--">-</button>
+
+</template>
+
+<style scoped>
+button {
+  background-color: #4CAF50;
+  /* Green */
+  margin: 0 10px;
+}
+</style>
