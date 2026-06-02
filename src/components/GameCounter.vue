56bc338690b13722c3c9082dd8bbf2cad1e5c2ce
Alex Zapata <alexzapata1984@gmail.com>
2026-06-02 15:37:42 +0200

    Add Estilos en Vue: Scoped, Clases Dinámicas y Uso de Sass

--- src/components/GameCounter.vue
+++ src/components/GameCounter.vue
@@ -1,22 +1,35 @@
 <script setup>
 
-console.log('componente de juego')
-
+const isActive = true
 
 </script>
 
 <template>
   <button @click="counter++">+</button>
 
-  <span>contador {{ counter }}</span>
+  <span class="counter" :class="{ 'counter--active': isActive }">contador: <span>0 - {{ counter }}</span></span>
 
   <button @click="counter--">-</button>
 
 </template>
 
-<style scoped>
+<style scoped lang="scss">
+.counter {
+  font-weight: bold;
+  font-size: 1.5em;
+  margin: 0 20px;
+
+  &>span {
+    color: #1f099b;
+  }
+}
+
+.counter--active {
+  color: #4CAF50;
+}
+
 button {
-  background-color: #4CAF50;
+  background-color: #a80b0b;
   /* Green */
   margin: 0 10px;
 }
